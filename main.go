c49e3f9bdbf30b29bc8e55ded466836734afb7ab
a.smyslov <a.smyslov@bft.ru>
2026-07-21 15:02:21 +0300

    Реестр переменных состояния

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"testapi/state"
 )
 
 func main() {
 	token, err := state.GetToken("test01")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		state.SaveState("test01", &state.StateEntry{Token: "12345", RefreshToken: "54321"})
 	}
 	fmt.Println(token)
 	// suite, err := conf.Load(os.Args[1])
